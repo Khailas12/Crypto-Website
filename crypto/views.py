9077becff89711bae1d22126fc8a0e3d3350a662
Khailas R <khailas303@gmail.com>
2021-11-13 19:45:38 +0530

    tables created in index

--- crypto/views.py
+++ crypto/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    context = {}
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false'  
+    
+    coins = requests.get(url).json()
+    context = {'coins': coins}
     return render(request, 'index.html', context)
